31f029a7e4fcc2a51500951eda002e142701329a
edancho <edan.cho@gmail.com>
2022-01-11 16:55:17 -0800

    Got responses

---
+++ progressCheck1.js
@@ -0,0 +1,17 @@
+let number1 = parseInt(prompt('Pick a number: '));
+let operation = (prompt('Pick an operation: '));
+let number2 = pareInt(prompt('Pick another number: '));
+
+switch(answer) {
+	case "+":
+		number1 + number2
+		break;
+    case "-":
+        number1-number2
+        break;
+    case "*":
+        number1*number2
+        break;
+    case "/":
+        number1/number2    
+}
