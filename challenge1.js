00dcb031f0014c6ede6538c0cbeee012aea98236
edancho <edan.cho@gmail.com>
2022-01-19 14:08:26 -0800

    polished up challenge 1

--- challenge1.js
+++ challenge1.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
-const data = fs.readFileSync('/Users/EdanCho/Downloads/PC1input.txt', 'UTF8');
-const numbers = data.split("\n");
+//const data = fs.readFileSync('/Users/EdanCho/Downloads/PC1input.txt', 'UTF8');
+//const numbers = data.split("\n");
+function edan(){
+    return(fs.readFileSync('/Users/EdanCho/Downloads/PC1input.txt', 'UTF8'));
+}
 let jumps = 0;
 let jumpSize = null;
 
+const data = edan()
+const numbers = data.split("\n")
 function lift(){
+
     for(let i =0; i < numbers.length; i++){
         if(numbers[i] < numbers[i+1]) {
             jumps ++;
