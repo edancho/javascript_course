3510c2ca588e5d1a0eed38b2df9e1f5559c379f6
edancho <edan.cho@gmail.com>
2022-01-23 21:48:29 -0800

    finished challenge 2

--- challenge1.js
+++ challenge1.js
@@ -1,29 +1,29 @@
 const fs = require('fs');
 //const data = fs.readFileSync('/Users/EdanCho/Downloads/PC1input.txt', 'UTF8');
 //const numbers = data.split("\n");
-function edan(){
-    return(fs.readFileSync('/Users/EdanCho/Downloads/PC1input.txt', 'UTF8'));
+function getData() {
+    return (fs.readFileSync('/Users/EdanCho/Downloads/PC1input.txt', 'UTF8'));
 }
 let jumps = 0;
 let jumpSize = null;
 
-const data = edan()
+const data = getData()
 const numbers = data.split("\n")
-function lift(){
+function lift() {
 
-    for(let i =0; i < numbers.length; i++){
-        if(parseInt(numbers[i]) < parseInt(numbers[i+1])) {
-            jumps ++;
+    for (let i = 0; i < numbers.length; i++) {
+        if (parseInt(numbers[i]) < parseInt(numbers[i + 1])) {
+            jumps++;
         }
-     }
+    }
 }
-function maxJump(){
-    for(let i= 0; i < numbers.length; i++){
-        if (numbers[i+1]-numbers[i]>jumpSize){
-            jumpSize = numbers[i+1]-numbers[i];
+function maxJump() {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i + 1] - numbers[i] > jumpSize) {
+            jumpSize = numbers[i + 1] - numbers[i];
 
-        } 
-    } 
+        }
+    }
 }
 lift();
 console.log("There are a total of", jumps, "jumps");
