0c8caa6c92eb6a6800d15e848eb529813f39e641
edancho <edan.cho@gmail.com>
2022-03-21 10:09:43 -0700

    finished nhlstats

--- NHLStats.js
+++ NHLStats.js
@@ -6,39 +6,13 @@ const prompt = promptPKG();
 //goal: find the average number of goals for a player in the nhl
 //goal: find the average number of games played for a player in the nhl
 
-let operation = prompt('Please enter a team id(1-50)');
+let operation = prompt('Please enter a player id(8478470-8478480)');
 
 async function getPlayer(id) {
     let rosterLength = 28;
-    for (i=1;i<28;i++){
-
-    }
     let resp = await axios.get('https://statsapi.web.nhl.com/api/v1/people/' + id)
     let data = resp.data
     console.log(data);
 }
-/*
-async function Team(id){
-    let resp = await axios.get('https://statsapi.web.nhl.com/api/v1/people/' + id)
-    let data = resp.data
-    console.log(data)
-    return{
 
-    }
-
-
-}
-*/
-
-/*
-console.log(await getHockeyData(8447400))
-async function getPlayerStats(){
-    let resp = await axios.get('https://statsapi.web.nhl.com/api/v1/people/8447400/stats')
-    let data = resp.data
-    console.log(data)
-    
-}
-console.log(await getPlayerStats());
-*/
-getPlayer(8478476);
-//result.data.teams.find(e =>)
+getPlayer(operation);
